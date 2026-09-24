1e6467cfb9794211ec1cbaf98dadfa4632abb38b
RanG-5649 <gaolange@gmail.com>
2026-09-24 22:59:06 +0800

    Update AntiQues.java
    fix(Settings): adapt device name check for Android 17

--- library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/systemsettings/AntiQues.java
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/systemsettings/AntiQues.java
@@ -19,6 +19,8 @@
 package com.sevtinge.hyperceiler.libhook.rules.systemsettings;
 
 import android.content.Context;
+import android.os.Build;
+import android.widget.EditText;
 
 import com.sevtinge.hyperceiler.libhook.base.BaseHook;
 
@@ -28,45 +30,68 @@ import io.github.lingqiqi5211.ezhooktool.xposed.java.IMethodHook;
 public class AntiQues extends BaseHook {
     @Override
     public void init() {
-        findAndHookMethod("com.android.settings.MiuiDeviceNameEditFragment", "onSave", new IMethodHook() {
-            @Override
-            public void before(HookParam param) {
-                boolean originalValue = getStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD");
-                setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", true);
-                setObjectExtra(param, "originalValue", originalValue);
-            }
-            @Override
-            public void after(HookParam param) {
-                boolean originalValue = (boolean) getObjectExtra(param, "originalValue");
-                setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", originalValue);
-            }
-        });
-        findAndHookMethod("com.android.settings.wifi.EditTetherFragment", "onSave", new IMethodHook() {
-            @Override
-            public void before(HookParam param) {
-                boolean originalValue = getStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD");
-                setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", true);
-                setObjectExtra(param, "originalValue", originalValue);
-            }
-            @Override
-            public void after(HookParam param) {
-                boolean originalValue = (boolean) getObjectExtra(param, "originalValue");
-                setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", originalValue);
-            }
-        });
-        findAndHookMethod("com.android.settings.DeviceNameCheckManager", "getDeviceNameCheckResult", Context.class, String.class, int.class, "com.android.settings.DeviceNameCheckManager$GetResultSuccessCallback", new IMethodHook() {
-            @Override
-            public void before(HookParam param) {
-                boolean originalValue = getStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD");
-                setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", true);
-                setObjectExtra(param, "originalValue", originalValue);
-            }
-            @Override
-            public void after(HookParam param) {
-                boolean originalValue = (boolean) getObjectExtra(param, "originalValue");
-                setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", originalValue);
-            }
-        });
+        if (Build.VERSION.SDK_INT >= 37) {
+            // HyperOS 4 reads this final field in onSave. Android 17 no longer lets
+            // the old hook change it, so use the existing save path directly.
+            findAndHookMethod("com.android.settings.MiuiDeviceNameEditFragment", "onSave", new IMethodHook() {
+                @Override
+                public void before(HookParam param) {
+                    Object fragment = param.getThisObject();
+                    EditText editText = (EditText) getObjectField(fragment, "mDeviceNameEdit");
+                    String name = editText.getText().toString().trim();
+                    callStaticMethod(fragment.getClass(), "onSaveImpl", name, fragment);
+                    param.setResult(null);
+                }
+            });
+            // In HyperOS 4, onSave only uses this result to choose whether to
+            // run the SSID check. The other branch still saves the new config.
+            findAndHookMethod("com.android.settings.wifi.EditTetherFragment", "isSoftApSsidchanged", new IMethodHook() {
+                @Override
+                public void before(HookParam param) {
+                    param.setResult(false);
+                }
+            });
+        } else {
+            findAndHookMethod("com.android.settings.MiuiDeviceNameEditFragment", "onSave", new IMethodHook() {
+                @Override
+                public void before(HookParam param) {
+                    boolean originalValue = getStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD");
+                    setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", true);
+                    setObjectExtra(param, "originalValue", originalValue);
+                }
+                @Override
+                public void after(HookParam param) {
+                    boolean originalValue = (boolean) getObjectExtra(param, "originalValue");
+                    setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", originalValue);
+                }
+            });
+            findAndHookMethod("com.android.settings.wifi.EditTetherFragment", "onSave", new IMethodHook() {
+                @Override
+                public void before(HookParam param) {
+                    boolean originalValue = getStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD");
+                    setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", true);
+                    setObjectExtra(param, "originalValue", originalValue);
+                }
+                @Override
+                public void after(HookParam param) {
+                    boolean originalValue = (boolean) getObjectExtra(param, "originalValue");
+                    setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", originalValue);
+                }
+            });
+            findAndHookMethod("com.android.settings.DeviceNameCheckManager", "getDeviceNameCheckResult", Context.class, String.class, int.class, "com.android.settings.DeviceNameCheckManager$GetResultSuccessCallback", new IMethodHook() {
+                @Override
+                public void before(HookParam param) {
+                    boolean originalValue = getStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD");
+                    setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", true);
+                    setObjectExtra(param, "originalValue", originalValue);
+                }
+                @Override
+                public void after(HookParam param) {
+                    boolean originalValue = (boolean) getObjectExtra(param, "originalValue");
+                    setStaticBooleanField(findClassIfExists("miui.os.Build"), "IS_INTERNATIONAL_BUILD", originalValue);
+                }
+            });
+        }
         findAndHookMethod("com.android.settings.bluetooth.MiuiBTUtils", "isSupportNameComplianceCheck", Context.class, new IMethodHook() {
             @Override
             public void before(HookParam param) {
